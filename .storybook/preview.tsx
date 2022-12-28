04db61dbc44fd7be498f778d0c9c6eb89ace3687
Raul Burigo <raul.burigo@gmail.com>
2022-12-28 12:49:13 -0300

    remove theme

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,6 +1,6 @@
 import { StoryFn } from '@storybook/react'
 import { ThemeProvider } from '../src/lib/ThemeProvider'
-import * as Docs from '@storybook/addon-docs'
+
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
@@ -11,37 +11,37 @@ export const parameters = {
   },
 }
 
-export const decorators = [
-  (Story: StoryFn, context: any) => (
-    <ThemeProvider colors={context.globals.theme}>
-      <Story />
-    </ThemeProvider>
-  ),
-]
+// export const decorators = [
+//   (Story: StoryFn, context: any) => (
+//     <ThemeProvider colors={context.globals.theme}>
+//       <Story />
+//     </ThemeProvider>
+//   ),
+// ]
 
-export const globalTypes = {
-  theme: {
-    name: 'Theme',
-    description: 'Global theme for components',
-    defaultValue: undefined,
-    toolbar: {
-      icon: 'circlehollow',
-      items: [
-        { value: undefined, icon: 'circlehollow', title: 'default merca' },
-        // aqui é possível adicionar temas de clientes para testar visualização
-        {
-          value: {
-            // primary: '#08244a',
-            primary: '#002040',
-            primaryText: '#ffffff',
-            secondary: '#fe1a25',
-            secondaryText: '#ffffff',
-          },
-          icon: 'circlehollow',
-          title: 'cliente1',
-        },
-      ],
-      showName: true,
-    },
-  },
-}
+// export const globalTypes = {
+//   theme: {
+//     name: 'Theme',
+//     description: 'Global theme for components',
+//     defaultValue: undefined,
+//     toolbar: {
+//       icon: 'circlehollow',
+//       items: [
+//         { value: undefined, icon: 'circlehollow', title: 'default merca' },
+//         // aqui é possível adicionar temas de clientes para testar visualização
+//         {
+//           value: {
+//             // primary: '#08244a',
+//             primary: '#002040',
+//             primaryText: '#ffffff',
+//             secondary: '#fe1a25',
+//             secondaryText: '#ffffff',
+//           },
+//           icon: 'circlehollow',
+//           title: 'cliente1',
+//         },
+//       ],
+//       showName: true,
+//     },
+//   },
+// }
